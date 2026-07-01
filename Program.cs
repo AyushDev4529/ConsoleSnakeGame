57606607d722499f0374029cfb938eba6e48212c
AyushDev4529 <gamedev4529@gmail.com>
2026-07-01 19:52:09 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleSnakeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ConsoleSnakeGame
+A 2D console-based Snake game built in pure C# featuring custom grid sizes, dynamic fruit spawning, and isolated input handling.
